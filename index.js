2d0d83e12369679158d3a8b09dc45074ed82f735
Frank <f.berrocalazofeif887@mybvc.ca>
2023-05-31 15:25:26 -0600

    Creation of JS file
    Moving script to different file

---
+++ index.js
@@ -0,0 +1,9 @@
+let display = "Hello World";
+console.log(display);
+//Required to display information.
+let array = [];
+for (let index = 0; index < 10; index++) {
+  array[index] = index;
+}
+
+console.log(array);
